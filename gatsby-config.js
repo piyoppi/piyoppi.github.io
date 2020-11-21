8da7c45bdc0415afae332a70f954e59249f5b272
piyoppi <garakuta.toolbox@gmail.com>
2020-11-21 12:16:38 +0900

    OGPなどの設定を追加する

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,16 @@ module.exports = {
   plugins: [],
   siteMetadata: {
     title: 'がらくたツールボックス',
+    siteName: 'がらくたツールボックス',
     description: 'ぴよっぴのつくったものたちをまとめています',
+    siteUrl: 'https://garakuta-toolbox.com',
+    twitter: {
+      card: 'summary_large_image',
+      image: 'https://garakuta-toolbox.com/og_image.png'
+    },
+    og: {
+      locale: 'ja_JP',
+      image: 'https://garakuta-toolbox.com/og_image.png'
+    }
   },
 }
