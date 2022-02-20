428c1962e080a550c80d87dc44a32edcdb20db0f
piyoppi <garakuta.toolbox@gmail.com>
2022-02-20 12:01:17 +0900

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,54 @@ module.exports = {
     "gatsby-remark-images",
     "gatsby-remark-prismjs",
     {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: {site, allMdx} }) => {
+              return allMdx.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  description: node.excerpt,
+                  date: node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + node.slug,
+                  guid: site.siteMetadata.siteUrl + node.slug,
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(sort: {order: DESC, fields: frontmatter___date}) {
+                  nodes {
+                    frontmatter {
+                      title
+                      date
+                    }
+                    slug
+                    excerpt(pruneLength: 100, truncate: true)
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "garakuta-toolbox weblog",
+            match: "^/weblog/",
+          },
+        ]
+      },
+    },
+    {
       resolve: "gatsby-source-filesystem",
       options: {
         name: 'blog',
